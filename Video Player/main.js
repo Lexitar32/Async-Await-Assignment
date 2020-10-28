e6338f2311092250641fc3e99ed3c0df3197a65d
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2020-10-29 00:59:04 +0100

    Changes added to the bideo player

--- Video Player/main.js
+++ Video Player/main.js
@@ -1,30 +1,41 @@
-createVideo("https://9jaflaverfiles.com/wp-content/uploads/2020/09/Davido-FEM-_Official-Video_.mp4").then(
-    playVideo()
-).catch(function (err) {
-    alert(err)
-});
+createVideo("https://sf-download.com/get.php?data=nkYWqkq6HJ9TfwFtMwgWMwdof29tMwS4fwDoujsWH2fUD3Fvnh9aih1SNVTbfSROF2RNkNftXLtWDQRQYGExYGYjYGYjnjsbD3gvMkYUnZ56fjF6MpstD2gvfZ5TfwDteUftD2gvfwRoM2tmM21JYGEjMwusD3gvYGEOeGDcemU1ehB2ENVJEpsaYGEONSRXM1d4V0tlELEzekyeemEeMjFZiWVJEpEsYGEOeGPsHaA1EN42lN45lZVJEpEbYGEOfZ1xFGtJDQxpkVD5kGFCFOYEG1ymDpfCM1gWL095GVt3u29cEO1zqhulEQB4eJVJEpEWM3bte0BJEhqteafzD2Yznpqte0BsemeteaVJBmOmEZVJEGYheGe1YGA1eUesemDteaVJBmOmEJVJEGYheGDcYGA1eUeJEhAteaVJBmA0eJVJEGYheaB0YGA1eUeJEhqteaVJBmA0lZVJEGYheaq4YGA2f2Fcnj1vYGEOikFcDQxmMZVJEQgvi3xanjsQf24te0FyMjqteaf2nNVmFhuEYGA2uQDte0BmeNVJEGYheaUteaf2uZVmFju0HGYcnQu5Ek81Hkfme2qteaVJB2u0HLFsfjsvuhubYGA2upqte0FzqZVJEGYhnjgcYGA2upVte0F2YGA2upSaYGEOEZVJEQRoYGEOeaBteafaujEWMky0iwEsfJVmFhAJeaVceZVJEpSsnjVte0BsYGA2upE2MZVmFjSaijsUYGA1eUfIMwE2YGA2npE4YGEOiksQYGA2Mk5vuZVmFhA4EGA3EaD0YGA2iwx4YGEOeGd2HaAmeJVJEp52DWVmFhO1lGA2lhPsEGB1eGq2eaAteaf2DWVmFhO2ehe5ead3lGOteaf1fkEsYGEOEZVJEQYvMwRzMpEtMZVmFjtvfJVJEp0te0FLFVAteafWu3Ote0B1EheJEheJYGA2f3Rznj92fJVmFjs3qkE4MZVJEGYhMjeteaVJB2EsYGA1eUEaiZVJEGYhM2EWM3yQYGA1eUEQfwx4MkcteaVJB3gvi3xanjsQf24teaVJB2SsnjVteaVJB3faupcteaVJB3yanZVJEGYhMk5vuZVJEGYhiwx4YGA1eUEoupAteafQD3bte0FxG2vcVVb4itYxNLybBaYtuLFCMRE5nhtYMRSEnVgVGhSgNjgQF0sSL1b4u3xRfmB2iQuaiafFB0tZGmSinSYVeku6G1F4iRb2qtSJnGgFVGd3GhV1uRxXFGYBMme1uwR6SVuaYGA2MpusM3gzupqte0F2nNVJEGYhuQDteaVJB3f0YGA1eUE2fJVJEGYhupVteaVJB3ftDJVJEGYhqk4teaVJB2E0D2YTiQFbq3RQYGA2MpuanWVmFORwe0EKu0RIVQyYnVRBlOtlFkfXkpB5E0T6GU15L3b5eV1xnaU4khfiG3SWS0FzL1ATVhtTF3SjBkERBkq0kLYbM3qmVRV4Etfrijuaemg0SLfxiGuVMwxsVSemSQyauSEWB2seqkqteaVmFZfzXLtWDQRQYGExYGYjYGYjnjsbD3gvMkYUnZ56fjF6MpstD2gvfZ5TfwDteUftD2gvfwRoM2tmM21JYGEjMwusD3gvYGEOeGDcemU1ehB2ENVJEpsaYGEONSRXM1d4V0tlELEzekyeemEeMjFZiWVJEpEsYGEOeGPsHaA1EN42lN45lZVJEpEbYGEOfZ1xFGtJDQxpkVD5kGFCFOYEG1ymDpfCM1gWL095GVt3u29cEO1zqhulEQB4eJVJEpEWM3bte0BJEGOteafQfwx4Mkcte0FyfwxWqwEvYGA2njsCqjE4MjuQMWVmFjtvfJVJEQf5YGEOE00teaf2uWVmFhesYGA1eUeJlNVJEQf0YGEOf3BTeQx6u3U1MmVTfQemfJVJEGYhf3BTuwRUMjs0E2dteaf2fJVmFj9cYGA1eUE4iwPteaf2fNVmFwDteaf2fkete0B0YGA2qk4te0BJEZVJEpE0D2YTiQFbq3RQYGEOeaAJEGPcYGA2fLR4fNVmFhOteaf2D3fvYGEOM3xbD2BteaVJFpyvq3Dteaf6D3dte0FyMjqteafTMps0YGEOemO0EmUmlNVJEpgcnZVmFhO4EW4JlhOteafoupAte0BsEGUJEaq3EhVJlhDsemB5YGA2upAte0BsEaPmlGA4EmUsYGA2ukSaqNVmFhBteafJMjssM25afkcte0FyMjqteafTYGEOS0SZYGA2DQusYGEOEGBseGAJeWVJEpusM3gzupqte0Fvu3RanjcteaVJB2saYGA1eUEaqNVJEGYhD2dteaVJB2EWM3bteaVJB2uUqwgTMZVJEGYhnjsCqjE4MjuQMWVJEGYhfLR4fNVJEGYhupE2MZVJEGYhnpE4YGA1eUETMps0YGA1eUEbqwdteaVJB252DWVJEpuanWVmFORXimxFNagIVtRYnp8mNLR5q0fCuR8TGR9EejtlFOusnjFNHLFKNp5xBpE3G01YfauHM2ExGO1hNSROSQxDEU5Hq2gfkQFXfU9pfj5iqQtDnV1zMLuWL3EZD1uFitxhF3tvM2SRBNVJEGEOYGA1e0Bteafof3Rznj92fJVmFwf5YGA1eUE2uWVJEGYhuQBteaVJB3fQYGA1eUE2fNVJEGYhupSaYGA1eUEsMWVJEGYhD3FaDp1Iujgmqkqteafof2E6YGEOBVqmB193BkyNnUt5BSP4NV5RfU9ifhU3N3ylGLtKnaUsGVR6lGgDEtyXukYLFj9KVW1Blk1wuVfxD0SxfmFfqpgzumEBSGd2SUybf2emlwFSuURyE1FvqwRFVmEknpE1V2YhMOssfJVJEGEOYpYQXGO2ehe5ead4EaqpD3O9eGP1HaOseW4meW4JeaUpf2E6XLe2E3SvMhSTEGSTlGA4MmFvlhRbEmD2MmSbEj02lLEzYpA9V2yvMjAteaxxuLgaMk90YGAcVQtyDQt2YGAcGjFUqB%3D%3D").then(
+           playVideo()
+        ).catch((error) => {
+            alert(error)
+        })
 
-async function createVideo(videoUrl) {
-     let promise = new Promise(function(resolve,reject){
-     let video = document.createElement('video')
-     let source = document.createElement('source')
-     video.setAttribute('controls', 'controls')
-     video.setAttribute('id','video')
-     
-     source.src=videoUrl
-     video.appendChild(source)
+        async function createVideo(videoSource) {
+          let checker = await new Promise((resolve, reject) => {
 
-     video.onload = () => resolve(video)
-     video.onerror = () => reject(new Error("video source is not available"))
-     
-     document.body.appendChild(video)
-     
-     })
-     let result = await promise;
-};
+                // Create audio and source
+                let video = document.createElement('video')
+                let source = document.createElement('source')
 
- function playVideo() {
-     return new Promise(function (resolve, reject) {
-         resolve(document.getElementById('video').play());
-     });
-};
+                // Set attributes
+                video.setAttribute('controls', 'controls')
+                video.setAttribute('id', 'video')
+
+                // Add src
+                source.src = videoSource
+                video.appendChild(source)
+
+                video.onload = () => resolve(video)
+
+                video.onerror = () => reject(new Error('Video source not found'))
+
+                document.body.appendChild(video) 
+            })
+        }
+
+
+        function playVideo() {
+            
+            return new Promise((resolve, reject) => {
+
+                setTimeout(() => {
+               x = document.getElementById("video")
+               resolve(x.play());
+                
+            }, 700);
+            })
+        }
